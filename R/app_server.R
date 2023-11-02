6edc2d0f72fc2b6086af537b4a9ce35e87398d39
mikkelnrasmussen <s193518@dtu.dk>
2023-11-02 10:21:25 +0000

    build: add app server script with modules

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,10 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+
+  # Module for generating peptide sequence from DNA
+  mod_dna_to_peptide_server("dna_to_peptide_1")
+
+  # Module for the visualization of the peptide sequence
+  mod_plot_peptide_seq_server("plot_peptide_seq_1")
 }
